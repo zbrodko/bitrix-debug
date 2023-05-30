afa61ac1bcd922c35c5ef34890a023fa7cdbee58
zbrodko <zbrodko@gmail.com>
2023-05-30 11:37:49 +0300

    Start

---
+++ local/php_interface/include/dev.php
@@ -0,0 +1,55 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+if (!function_exists('logVar')){
+	function logVar($var, $string="", $filename="log.txt") {
+		define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/".$filename);
+		if (is_array($var)) {
+			AddMessage2Log($string." = ".var_export($var,true));
+		} else {
+			AddMessage2Log($string." = ".$var);
+		}
+	}
+}
+
+if (!function_exists('console')) {
+	function console($var, $label = '', $useBackTrace = true, $toString = false) {
+	    if ($useBackTrace) {
+	        $backTrace = debug_backtrace();
+	    }
+	    $js = '<script type="text/javascript">
+	      console.log(' . (empty($label) ? '' : '\'' . $label . '\',') . json_encode($var) . ');
+	      ' . ($useBackTrace ? 'console.log("line: ' . $backTrace[0]['line'] . ', file: ' . $backTrace[0]['file'] . '")' : '') . ';
+	    </script>';
+	    if (!$toString) {
+	        echo $js;
+	        return '';
+	    } else {
+	        return $js;
+	    }
+	}
+}
+
+class Timer
+{
+    /**
+     * @var float время начала выполнения скрипта
+     */
+    private static $start = .0;
+
+    /**
+     * Начало выполнения
+     */
+    static function start()
+    {
+        self::$start = microtime(true);
+    }
+
+    /**
+     * Разница между текущей меткой времени и меткой self::$start
+     * @return float
+     */
+    static function finish()
+    {
+        return microtime(true) - self::$start;
+    }
+}
---
+++ local/php_interface/init.php
@@ -0,0 +1,3 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+require_once(__DIR__ . '/include/dev.php');
+?>
